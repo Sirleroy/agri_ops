55c960f3c76bebcb9763772724333a5db6875b0e
Ezinna Ohah <ohahezinna@gmail.com>
2026-03-13 12:54:20 +0100

    feat: add login/logout UI, wire auth URLs, add LoginRequiredMixin to all views

--- agri_ops_project/urls.py
+++ agri_ops_project/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Authentication
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
+
+    # Application
     path('', include('apps.dashboard.urls', namespace='dashboard')),
     path('inventory/', include('apps.inventory.urls', namespace='inventory')),
     path('products/', include('apps.products.urls', namespace='products')),
